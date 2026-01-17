c1a15c1e4d981324f629f906ef2ae1052addfebc
qiuzhenhao <qzhsdream@163.com>
2026-01-17 10:43:55 +0800

    feat: structures

---
+++ assets/Scripts/UI/StartMenu.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('StartMenu')
+export class StartMenu extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
